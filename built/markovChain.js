17c7a3bae109eb10866845da189f1e1046781890
Simone Kovacs <dsimonekovacs@gmail.com>
2019-01-27 13:34:58 -0500

    update markovChain

--- built/markovChain.js
+++ built/markovChain.js
@@ -2,18 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 function markovChain(textArray) {
     var markovObject = {};
-    // if (textArray.length = 0) {
-    //     return undefined;
-    // } else {
-    //     let word = textArray
-    // }
     for (var i = 0; i < textArray.length; i++) {
         var word = textArray[i];
         var nextWords = [];
         var newWord = textArray.slice(i + 1, i + 2);
         nextWords.push(newWord);
-        [].concat.apply([], nextWords);
-        markovObject.word = nextWords;
+        // let newArray = [].concat.apply([], nextWords);
+        markovObject[word] = nextWords;
     }
     return markovObject;
 }
--- src/markovChain.ts
+++ src/markovChain.ts
@@ -5,20 +5,14 @@ interface markovObject {
 export default function markovChain(textArray) {
     let markovObject: markovObject = {};
 
-    // if (textArray.length = 0) {
-    //     return undefined;
-    // } else {
-    //     let word = textArray
-    // }
-
     for (let i = 0; i < textArray.length; i++) {
         let word = textArray[i];
         let nextWords = [];
 
         let newWord = textArray.slice(i + 1, i + 2);
         nextWords.push(newWord);
-        [].concat.apply([], nextWords);
-        markovObject.word = nextWords;
+        let newArray = [].concat.apply([], nextWords);
+        markovObject[word] = newArray;
     }
     return markovObject;
 }
